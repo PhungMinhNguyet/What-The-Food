371638bedbbfd42014c60713a9e10070a1eba0ba
Dao Vinh Linh <linh142000@gmail.com>
2019-11-10 14:47:40 +0700

    Recipe page

---
+++ recipes/meals.js
@@ -0,0 +1,12 @@
+function showhide() {
+    var x = document.getElementById("recommend-tab");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+
+
+
