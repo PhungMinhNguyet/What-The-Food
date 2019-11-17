9ebe50a7eaf2629774ae892be62f279404da57b1
PhungMinhNguyet <hnts.phungminnguyet@gmail.com>
2019-11-18 00:53:41 +0700

    update not found meal

--- searchPage/search.js
+++ searchPage/search.js
@@ -12,19 +12,29 @@ input.addEventListener("keyup", function(event) {
     }
 })
 async function data() {
-    const url = 'https://www.themealdb.com/api/json/v1/1/search.php?s=' + input.value;
-    const conn = await fetch(url);
-    const data = await conn.json();
-    console.log("data: ", data);
-    sample.textContent = "";
-    input.value = "";
-    showData(data);
+    if (input.value == "") {
+        sample.textContent = "";
+        input.value = "";
+        sample.insertAdjacentHTML('beforeend', '<h2>No Meals found</h2>')
+    } else {
+        const url = 'https://www.themealdb.com/api/json/v1/1/search.php?s=' + input.value;
+        const conn = await fetch(url);
+        const data = await conn.json();
+        console.log("data: ", data);
+        sample.textContent = "";
+        input.value = "";
+        if (data.meals == null) {
+            sample.insertAdjacentHTML('beforeend', `<h2>No Meals Found</h2>`)
+        }
+        showData(data);
+    }
 }
 
 function showData(data) {
     console.log(data.meals.length);
     for (let i = 0; i < data.meals.length; i++) {
         let id = data.meals[i].idMeal
-        sample.insertAdjacentHTML('beforeend', `<div class="result" id="${id}""><a href="../detail/detail.html?id=${id}" class="text"><img class="image" src="${data.meals[i].strMealThumb}" alt='food.pic'><div id="strMeal">${data.meals[i].strMeal}</a></div>`)
+        sample.insertAdjacentHTML('beforeend', `<div class="result" id="${id}""><a href="../detail/detail.html?id=${id}" class="text">
+            <img class="image" src="${data.meals[i].strMealThumb}" alt='food.pic'><div id="strMeal">${data.meals[i].strMeal}</a></div>`)
     }
 }
